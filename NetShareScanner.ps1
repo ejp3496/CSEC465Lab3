1b0f5b2f19aa4fda39070c9177213823a60f8e4b
ejp3496 <43967089+ejp3496@users.noreply.github.com>
2018-10-09 20:50:39 -0400

    Add files via upload

---
+++ NetShareScanner.ps1
@@ -0,0 +1,23 @@
+﻿$netviewout = net view
+$devices =  New-Object System.Collections.Generic.List[System.Object]
+
+foreach ($line in $netviewout){
+    #$line = ($netviewout.split("\n"))[0] 
+    #write-host $line 
+    if( $line.startswith("\\"))
+    {
+        $devices.Add($line)
+    }
+}
+
+foreach ($device in $devices)
+{
+   write-host $device.Trim()  ":"
+   net view $device.Trim() 
+}
+
+Read-Host -Prompt "Press Enter to exit"
+
+
+    
+
